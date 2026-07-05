98109d198897975c7e2cd9f98d3178f1b6e04cc2
jeongmin4869 <ljm4135@naver.com>
2026-07-05 22:08:29 +0900

    feat: login 방어로직 추가

--- Course/Inflearn/실무를위한올인원기본기/9.아키텍처/architecture-sample/app/application/interface/user_repository.py
+++ Course/Inflearn/실무를위한올인원기본기/9.아키텍처/architecture-sample/app/application/interface/user_repository.py
@@ -7,3 +7,6 @@ class AbstractRepository(abc.ABC):
 
     @abc.abstractmethod
     def create(self, model: Domain): ...
+
+    @abc.abstractmethod
+    def find_one(self, model: Domain): ...
--- Course/Inflearn/실무를위한올인원기본기/9.아키텍처/architecture-sample/app/application/service/user_service.py
+++ Course/Inflearn/실무를위한올인원기본기/9.아키텍처/architecture-sample/app/application/service/user_service.py
@@ -9,5 +9,8 @@ class UserService:
     def create_user(self, user_name: str):
         # 데이터베이스 저장
         _user = User(name=user_name)
+        if self.repository.find_one(model=_user):
+            raise ValueError("유저가 이미 존재한다")
         user = self.repository.create(_user)
+
         return user
